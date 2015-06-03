92ae570dcf53b0b592e29033e5558fdfe923be58
Jacob Smith <jacob@jacobsmith.io>
2015-06-03 11:38:06 -0400

    Implement basic routing and login

--- TODO.md
+++ TODO.md
@@ -2,9 +2,9 @@
   - [x] HTML structure and layout for login screen
   - [x] Respond to URL changes by loading login on '/' and a (for now) empty chat screen on '/chat'
   - [ ] HTML structure and layout for chat screen
-- [ ] Implement login
-  - [ ] Save the user data in a variable so it can be added to messaged
-  - [ ] Redirect to chat screen upon login
+- [x] Implement login
+  - [x] Save the user data in a variable so it can be added to message
+  - [x] Redirect to chat screen upon login
 - [ ] Log a list of messages from the server
 - [ ] Display a list of messages from the server
 - [ ] Implement form to create a new message
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 (function(){
   'use strict';
 
+  var username = '';
+
   $(document).ready(function(){
 
     route();
 
     $(document).on('submit', '.login-form', function(event){
       event.preventDefault();
+      username = $(this).find('.login-form-username').val();
       window.location.hash = '/chat';
     });
 
@@ -22,9 +25,14 @@
         $('.application').html(JST['login']());
         break;
       case '#/chat':
-        $('.application').html(JST['chat']());
+        renderChat();
         break;
     }
+  }
+
+  function renderChat() {
+    $('.application').html(JST['chat']());
+    console.log('username:', username);
   }
 
 })();
